e094d2694ae75b7eff9a410dfdcbc1c9377c8c51
Prakash Raut <prakash_raut@outlook.com>
2025-09-04 11:07:29 +0530

    Add customizable Tag prop to Tooltip component

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,19 @@ import { useState } from "react";
 
 import './../styles/App.css';
 
-const Tooltip = ({ text, children }) => {
+
+const Tooltip = ({ text, children, Tag = "p" }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div
+    <Tag
       className="tooltip-container"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       {children}
       {isHovered && <div className="tooltip-text">{text}</div>}
-    </div>
+    </Tag>
   );
 };
 
@@ -23,20 +24,22 @@ const arr = [
   {
     id: 1,
     text: "This is a tooltip",
-    children: "Hover over me"
+    children: "Hover over me",
+    Tag: "h2"
   },
   {
     id: 2,
     text: "This is another tooltip",
-    children: "Hover over me to see another tooltip"
+    children: "Hover over me to see another tooltip",
+    Tag: "p"
   }
-]
+] 
 
 const App = () => {
   return (
     <div>
       {arr.map((item) => (
-        <Tooltip key={item.id} text={item.text} >
+        <Tooltip key={item.id} text={item.text} Tag={item.Tag}>
           {item.children}
         </Tooltip>
       ))}
